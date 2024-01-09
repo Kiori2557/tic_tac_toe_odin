d4418cd7601bec34efc78b2066c7c32da423c54a
Aung Khant Paing <akpaing99@gmail.com>
2024-01-09 14:40:18 +0630

    Added html, css and js
    added html skeleton and basic css
    added js gameBoard, gameBoardController, screenController and
        function to check winning conditions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const gameBoard = function () {
+const gameBoard = (function () {
   const rows = 3;
   const columns = 3;
   const board = [];
@@ -6,14 +6,14 @@ const gameBoard = function () {
     let row = [];
     board.push(row);
     for (let j = 0; j < columns; j++) {
-      row.push(0);
+      row.push("");
     }
   }
   return { board };
-};
+})();
 
 const gameBoardController = (function () {
-  const game = gameBoard();
+  const game = gameBoard;
   const board = game.board;
   let playerOne = "playerOne";
   let playerTwo = "playerTwo";
@@ -24,7 +24,7 @@ const gameBoardController = (function () {
     },
     {
       name: playerTwo,
-      mark: "y",
+      mark: "o",
     },
   ];
   let activePlayer = players[0];
@@ -35,9 +35,83 @@ const gameBoardController = (function () {
   const play = function (x, y) {
     let playerMark = activePlayer.mark;
     board[x][y] = playerMark;
+    checkWinner();
     changePlayer();
     console.log(`${activePlayer.name}'s turn.`);
     console.log(board);
   };
-  return { game, play };
+  return { play, activePlayer };
 })();
+
+//Screen Controller
+
+const screenController = (function () {
+  const boardDiv = document.querySelector(".gameBoard");
+  const playerTurnDiv = document.querySelector(".playerTurn");
+  const game = gameBoard;
+  const board = game.board;
+  const gameController = gameBoardController;
+
+  // generateBoard
+  const generateBoard = function () {
+    boardDiv.textContent = "";
+    board.forEach((row, x) =>
+      row.forEach((value, y) => {
+        const cell = document.createElement("button");
+        cell.dataset.position = [x, y];
+        cell.classList.add("cell");
+        boardDiv.appendChild(cell);
+        cell.textContent = value;
+      })
+    );
+  };
+
+  boardDiv.addEventListener("click", playARound);
+
+  function playARound(e) {
+    let cell = e.target;
+    if (cell.textContent == "x" || cell.textContent == "o") return;
+    let xyPos = cell.dataset.position.split(",");
+    let x = xyPos[0];
+    let y = xyPos[1];
+    gameController.play(x, y);
+    generateBoard();
+  }
+
+  return { generateBoard };
+})();
+screenController.generateBoard();
+
+//check winning condition
+function checkWinner() {
+  const game = gameBoard;
+  const board = game.board;
+  const columns = {};
+  const rows = {};
+  createColumn(board);
+  createRow(board);
+  function createColumn(board) {
+    board.forEach((row) => {
+      row.forEach((cell, index) => {
+        if (cell != "") {
+          if (!columns[`column${index}`]) {
+            columns[`column${index}`] = [];
+          }
+          columns[`column${index}`].push(cell);
+        }
+      });
+    });
+  }
+  function createRow(board) {
+    board.forEach((row, index) => {
+      row.forEach((cell) => {
+        if (cell != "") {
+          if (!rows[`row${index}`]) {
+            rows[`row${index}`] = [];
+          }
+          rows[`row${index}`].push(cell);
+        }
+      });
+    });
+  }
+}

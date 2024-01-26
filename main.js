51cd19f1cfbc8208852853d361d2755fc4ac2810
Aung Khant Paing <akpaing99@gmail.com>
2024-01-26 14:58:41 +0630

    finish the js winning conditions and basic styling

--- main.js
+++ main.js
@@ -27,20 +27,30 @@ const gameBoardController = (function () {
       mark: "o",
     },
   ];
+  const score = {
+    playerOne: 0,
+    playerTwo: 0,
+  };
+
+  const addScore = (player) => score[player]++;
+  const getScore = () => score;
+
   let activePlayer = players[0];
   const changePlayer = function () {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
+  const getActivePlayer = () => activePlayer;
+
   const play = function (x, y) {
     let playerMark = activePlayer.mark;
     board[x][y] = playerMark;
-    checkWinner();
+    checkWinner(x, y);
     changePlayer();
     console.log(`${activePlayer.name}'s turn.`);
     console.log(board);
   };
-  return { play, activePlayer };
+  return { play, getActivePlayer, addScore, getScore };
 })();
 
 //Screen Controller
@@ -64,9 +74,20 @@ const screenController = (function () {
         cell.textContent = value;
       })
     );
+    renderPlayerTurn();
   };
 
   boardDiv.addEventListener("click", playARound);
+
+  function currentPlayer() {
+    return gameController.getActivePlayer();
+  }
+
+  function renderPlayerTurn() {
+    playerTurnDiv.textContent = "";
+    let activePlayer = currentPlayer();
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn`;
+  }
 
   function playARound(e) {
     let cell = e.target;
@@ -83,35 +104,88 @@ const screenController = (function () {
 screenController.generateBoard();
 
 //check winning condition
-function checkWinner() {
+function checkWinner(x, y) {
   const game = gameBoard;
   const board = game.board;
-  const columns = {};
-  const rows = {};
-  createColumn(board);
-  createRow(board);
-  function createColumn(board) {
+  const gameController = gameBoardController;
+  const columns = [];
+  const rows = [];
+  const crosses = [];
+  let hasWinner = false;
+  createColumnObj(board);
+  createRowObj(board);
+  createCrossObj(board);
+  console.log(columns);
+  console.log(rows);
+  console.log(crosses);
+  function check(obj) {
+    let currentPlayer = gameController.getActivePlayer();
+    obj.forEach((array) => {
+      if (array.length == 3) {
+        hasWinner = array.every((cell) => cell == currentPlayer.mark);
+        return hasWinner;
+      }
+    });
+  }
+  check(columns);
+  check(rows);
+  check(crosses);
+  if (hasWinner) {
+    let winner = gameController.getActivePlayer().name;
+    gameController.addScore(winner);
+    let score = gameController.getScore();
+    console.log(
+      `the score is updated to playerOne:${score.playerOne} and playerTwo:${score.playerTwo} `
+    );
+  }
+  // function columnObj(x, y) {
+  //   let cell = board[x][y];
+  //   if (!columns[y]) {
+  //     columns[y] = [];
+  //   }
+  //   columns[y].push(cell);
+  // }
+  // function rowObj(x, y) {
+  //   let cell = board[x][y];
+  //   if (!rows[x]) {
+  //     rows[x] = [];
+  //   }
+  //   rows[x].push(cell);
+  // }
+  function createColumnObj(board) {
     board.forEach((row) => {
       row.forEach((cell, index) => {
         if (cell != "") {
-          if (!columns[`column${index}`]) {
-            columns[`column${index}`] = [];
+          if (!columns[index]) {
+            columns[index] = [];
           }
-          columns[`column${index}`].push(cell);
+          columns[index].push(cell);
         }
       });
     });
   }
-  function createRow(board) {
+  function createRowObj(board) {
     board.forEach((row, index) => {
       row.forEach((cell) => {
         if (cell != "") {
-          if (!rows[`row${index}`]) {
-            rows[`row${index}`] = [];
+          if (!rows[index]) {
+            rows[index] = [];
           }
-          rows[`row${index}`].push(cell);
+          rows[index].push(cell);
         }
       });
     });
+  }
+  function createCrossObj(board) {
+    if (board[1][1]) {
+      let [cell1, cell2, cell3] = [board[0][0], board[1][1], board[2][2]];
+      let [cell4, cell5, cell6] = [board[0][2], board[1][1], board[2][0]];
+      if (cell1 != "" && cell2 != "" && cell3 != "") {
+        crosses[0] = [cell1, cell2, cell3];
+      }
+      if (cell4 != "" && cell5 != "" && cell6 != "") {
+        crosses[1] = [cell4, cell5, cell6];
+      }
+    }
   }
 }
